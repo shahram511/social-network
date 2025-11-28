8ebd7fb0569f1bc91cc506b2798c606df3fe8b3a
shahram <azghandishahram@gmail.com>
2025-11-28 23:08:40 +0330

    gt-branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# social-network
-## this is writen just whit python by using rest framwork in django
--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from accounts.models import Profile
-from django.contrib import admin
-
-
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'country', 'avatar']
